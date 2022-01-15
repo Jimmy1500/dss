d5344b08c47c543aa92165a2ec2f887d9cd7a5b3
James Ding <james.ding@traivefinance.com>
2022-01-15 15:56:40 -0600

    updated

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const { NODE, REDIS, AWS, GIT } = require('./src/Config')
+const { ENV, REDIS, AWS, GIT } = require('./src/Config')
 const { Bus } = require('./src/Bus')
 
-const bus = new Bus();
-bus.connect(
-    {
-		port: REDIS.PORT,
-		host: REDIS.HOST,
-		db: 0,
-		// username:
-		// password:
-    }
-)
+// const bus = new Bus();
+// bus.connect({
+//     port: REDIS.PORT,
+//     host: REDIS.HOST,
+//     db: 0,
+//     // username:
+//     // password:
+// });
 
 // bus.set('token', ['a', 'b', 'c'], false).then(
 //     bus.get('token', false).then( val => {
@@ -19,11 +17,11 @@ bus.connect(
 //     })
 // );
 
-bus.push(REDIS.TOPIC.M3_USER, [ 'user', 'REQ_USER'  ])
-bus.push(REDIS.TOPIC.M3_REPO, [ 'repos', 'REQ_REPOS'])
-
-bus.pull([...Object.values(REDIS.TOPIC)], [ 0, 0 ], 10)
-.then(last_id => {
-    console.log('last:', last_id);
-    bus.flush();
-})
+// bus.push(REDIS.TOPIC.M3_USER, [ 'user', 'REQ_USER'  ])
+// bus.push(REDIS.TOPIC.M3_REPO, [ 'repos', 'REQ_REPOS'])
+//
+// bus.pull([...Object.values(REDIS.TOPIC)], [ 0, 0 ], 10)
+// .then(last_id => {
+//     console.log('last:', last_id);
+//     bus.flush();
+// })
--- src/Config.js
+++ src/Config.js
@@ -1,21 +1,23 @@
 'use strict'
 
-const REDIS = {
+const ENV = {
+    NODE:                process.env.NODE_ENV             || 'LOCAL',
+}
+
+const REDIS = { 
+    HOST:   process.env.REDIS_HOST || 'localhost',
+    PORT:   process.env.REDIS_PORT || '6379',
     TOPIC: {
         M3_USER: 'M3_USER',
         M3_REPO: 'M3_REPO',
     },
-    HOST:   process.env.REDIS_HOST || 'localhost',
-    PORT:   process.env.REDIS_PORT || '6379',
 }
 
 const AWS = {
     REGION:             process.env.AWS_REGION            || 'us-east-1',
     ACCESS_KEY_ID:      process.env.AWS_ACCESS_KEY_ID     || 'FAKE',
     SECRET_ACCESS_KEY:  process.env.AWS_SECRET_ACCESS_KEY || 'FAKE',
-    API: {
-        VERSION:        process.env.AWS_API_VERSION       || '2006-03-01',
-    },
+    API_VERSION:        process.env.AWS_API_VERSION       || '2006-03-01',
     S3: {
         HOST:           process.env.AWS_S3_HOST           || 'http://localhost:4566',
         BUCKET:         process.env.AWS_S3_BUCKET         || 'm3-api',
@@ -26,12 +28,10 @@ const GIT = {
     ACCESS_TOKEN:       process.env.GIT_ACCESS_TOKEN      || 'FAKE',
 }
 
-const NODE = {
-    ENV:                process.env.NODE_ENV              || 'LOCAL',
-}
+
 
 module.exports = {
-    NODE,
+    ENV,
     REDIS,
     AWS,
     GIT,
